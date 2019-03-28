13ea0124794157618e71c96e1c2e6658de769ab2
Dominik Burczynski <dburczyn@BOC.Group>
2019-03-28 14:19:45 +0100

    added attributes

--- brokerage_js.js
+++ brokerage_js.js
@@ -1,7 +1,17 @@
-var authorizationtoken = "d24f49c6f91d500e54b341e678e6d80144c0c518";
+var authorizationtoken = ""; //store it securely, not in plaintext here !!
 var repourl = "https://api.github.com/repositories/175385549/contents/js"; // has to be exact, because link with username and repo name redirect, which is not handles properly by firefox and edge (chrome works ok)
 templatejson = {
   "title": "templatetitle"
+};
+var locale = "en-GB";
+var options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric'
 };
 function getSafe(fn, defaultVal) {
   try {
@@ -10,7 +20,7 @@ function getSafe(fn, defaultVal) {
     return defaultVal;
   }
 }
- function readCookie(name) {
+function readCookie(name) {
   var nameEQ = encodeURIComponent(name) + "=";
   var ca = document.cookie.split(';');
   for (var i = 0; i < ca.length; i++) {
@@ -21,8 +31,7 @@ function getSafe(fn, defaultVal) {
   return null;
 }
 $(function () {
-
-  $('#tile_form').on('submit', function (e) {
+  $('#getevents').on('click', function (e) {
     e.preventDefault();
     $("#foo").empty();
     $.ajax({
@@ -32,13 +41,15 @@ $(function () {
       },
       dataType: 'json',
       success: function (results) {
-              resultsJSON = [];
+        resultsJSON = [];
         $.each(results, function (i, f) {
           var splitname = f.name.split("_");
           resultsJSON.push({
             updatedat: splitname[0],
             createdat: splitname[1],
-            name: splitname[2]
+            datetype: splitname[2],
+            name: splitname[3],
+            type: splitname[4]
           });
         });
         if (document.getElementById('nameasc').checked) {
@@ -73,15 +84,14 @@ $(function () {
           });
         }
         resultsJSON.forEach(function (entry) {
-var name = entry.updatedat +"_"+ entry.createdat +"_"+ entry.name;
-          var parsedcreatedat = new Date(parseInt(entry.createdat));
-          var parsedupdatedat = new Date(parseInt(entry.updatedat));
-          var list = '<div class="col-md-3 cms-boxes-outer">                <div class="cms-boxes-items cms-features">                  <div class="boxes-align" data-toggle="modal" data-target="#expandedTile" id="' + name + '">                    <div class="small-box">                      <i class="fa fa-4x fa-laptop">&nbsp;</i>                        <p>' + entry.name + '</p><p> updated at: ' + parsedupdatedat + '</p><p> created at: ' + parsedcreatedat + '</p>                    </div>                  </div>                </div>              </div>  ';
+          var name = entry.updatedat + "_" + entry.createdat + "_" + entry.datetype + "_" + entry.name + "_" + entry.type;
+          var parsedcreatedat = new Date(parseInt(entry.createdat)).toLocaleDateString(locale, options);
+          var parsedupdatedat = new Date(parseInt(entry.updatedat)).toLocaleDateString(locale, options);
+          var list = '<div class="col-md-3 cms-boxes-outer">                <div class="cms-boxes-items cms-features" style="background-color:#c4d1cf">                  <div class="boxes-align" data-toggle="modal" data-target="#expandedTile" id="' + name + '">                    <div class="small-box">             <br>         <i class="far fa-calendar-alt fa-3x">&nbsp;</i>                        <h3>' + entry.name + '</h3><h3> Date: ' + entry.datetype + '</h3><h3>Last update: ' + parsedupdatedat + '</h3><h3> Created: ' + parsedcreatedat + '</h3>                    </div>                  </div>                </div>              </div>  ';
           $(list).appendTo("#foo");
         });
       },
     });
-
   });
   $('.container-fluid').on('click', '.boxes-align', function (e) {
     e.stopPropagation();
@@ -95,8 +105,13 @@ var name = entry.updatedat +"_"+ entry.createdat +"_"+ entry.name;
           currentresponse = response;
           content = atob(response.content);
           unencodedcontent = getSafe(() => JSON.parse(content), templatejson);
-          var title = unencodedcontent.title; // will not throw exception if templatejson is properly defined
-          document.getElementById('page-title').innerHTML = title;
+          document.getElementById('modal-title').innerHTML = response.name.split("_")[3];
+          document.getElementById('datetype').innerHTML = response.name.split("_")[2];
+          document.getElementById('dateofcreate').innerHTML = new Date(parseInt(response.name.split("_")[1])).toLocaleDateString(locale, options);
+          document.getElementById('dateofupdate').innerHTML = new Date(parseInt(response.name.split("_")[0])).toLocaleDateString(locale, options);
+          document.getElementById('currentPhoto').src = unencodedcontent.picture;
+          document.getElementById('mailbutton').href = 'mailto:' + unencodedcontent.email;
+          document.getElementById('description').innerHTML = unencodedcontent.description;
           $('#expandedTile').modal('show');
         }
       })
@@ -106,15 +121,27 @@ var name = entry.updatedat +"_"+ entry.createdat +"_"+ entry.name;
   });
 });
 $(function () {
-  $('#file_create_form').on('submit', function (e) {
+  $('#event_create_form').on('submit', function (e) {
     e.preventDefault();
+    var content = {};
+    content.description = $('#filecontent').val();
+    content.picture = $('#eventpicture').val();
+    content.email = $('#emailaddress').val();
+    var typesradio = document.getElementsByName('type');
+    var type;
+    for (var i = 0, length = typesradio.length; i < length; i++) {
+      if (typesradio[i].checked) {
+        type = typesradio[i].value;
+        break;
+      }
+    }
     $.ajax({
-      url: repourl + '/' + $('#createdat').val() + '_' + $('#createdat').val() + '_' + $('#filename').val(),
+      url: repourl + '/' + $('#createdat').val() + '_' + $('#createdat').val() + '_' + $('#eventdate').val() + '_' + $('#filename').val() + '_' + type,
       beforeSend: function (request) {
         request.setRequestHeader("Authorization", "token " + authorizationtoken);
       },
       type: 'PUT',
-      data: '{"message": "create file","content":"' + btoa($('#filecontent').val()) + '" }',
+      data: '{"message": "create file","content":"' + btoa(JSON.stringify(content)) + '" }',
       success: function (data) {
         alert('File create was performed. Passed data: ' + JSON.stringify(data));
         $('#createform').modal('hide');
@@ -126,8 +153,8 @@ $(function () {
   $('#eventdelete').on('click', function (e) {
     e.preventDefault();
     var action = confirm('Are you sure you wish to delete this item ? It cannot be undone!');
-    if(action === false) {
-       return false; // this would do
+    if (action === false) {
+      return false;
     }
     $.ajax({
       url: repourl + '/' + currentresponse.name,
@@ -148,9 +175,19 @@ $(function () {
     updatedat = new Date().getTime();
     e.preventDefault();
     document.getElementById("updatedatedited").value = updatedat;
-    document.getElementById("createdatedited").value = getSafe(() => currentresponse.name.split(/_(.+)/)[1].split(/_(.+)/)[0], updatedat);
-    document.getElementById("filenameedited").value = getSafe(() => currentresponse.name.split(/_(.+)/)[1].split(/_(.+)/)[1], 'default');
-    document.getElementById("filecontentedited").value = content;
+    document.getElementById("createdatedited").value = currentresponse.name.split("_")[1];
+    document.getElementById("filenameedited").value = currentresponse.name.split("_")[3];
+    document.getElementById("eventdateedited").value = currentresponse.name.split("_")[2];
+    document.getElementById("filecontentedited").value = unencodedcontent.description;
+    document.getElementById("emailaddressedited").value = unencodedcontent.email;
+    document.getElementById("eventpictureedited").value = unencodedcontent.picture;
+    var typesradionew = document.getElementsByName('typeedited');
+    for (var i = 0, length = typesradionew.length; i < length; i++) {
+      if (currentresponse.name.split("_")[4] === typesradionew[i].value) {
+        typesradionew[i].checked = true;
+        break;
+      }
+    }
   });
 });
 $(function () {
@@ -160,15 +197,36 @@ $(function () {
     document.getElementById("createdat").value = createdat;
     document.getElementById("filename").value = "";
     document.getElementById("filecontent").value = "";
+    document.getElementById("eventdate").value = "";
+    document.getElementById("emailaddress").value = "";
+    document.getElementById("eventpicture").value = "";
+    var typesradionew = document.getElementsByName('type');
+    for (var i = 0, length = typesradionew.length; i < length; i++) {
+      if (typesradionew[i].checked) {
+        typesradionew[i].checked = false;
+      }
+    }
   });
 });
 $(function () {
   $('#file_edit_form').on('submit', function (e) {
     e.preventDefault();
-    var dataforcreate = '{"message": "edit file","content":"' + btoa($('#filecontentedited').val()) + '" }';
+    var contentedited = {};
+    contentedited.description = $('#filecontentedited').val();
+    contentedited.picture = $('#eventpictureedited').val();
+    contentedited.email = $('#emailaddressedited').val();
+    var dataforcreate = '{"message": "edit file","content":"' + btoa(JSON.stringify(contentedited)) + '" }';
     var datafordelete = '{"message": "delete file","sha":"' + currentresponse.sha + '" }';
+    var typesradioedited = document.getElementsByName('typeedited');
+    var typeedited;
+    for (var i = 0, length = typesradioedited.length; i < length; i++) {
+      if (typesradioedited[i].checked) {
+        typeedited = typesradioedited[i].value;
+        break;
+      }
+    }
     $.ajax({
-      url: repourl + '/' + $('#updatedatedited').val() + '_' + getSafe(() => currentresponse.name.split(/_(.+)/)[1].split(/_(.+)/)[0], updatedat) + '_' + $('#filenameedited').val(),
+      url: repourl + '/' + $('#updatedatedited').val() + '_' + currentresponse.name.split("_")[1] + '_' + $('#eventdateedited').val() + '_' + $('#filenameedited').val() + '_' + typeedited,
       beforeSend: function (request) {
         request.setRequestHeader("Authorization", "token " + authorizationtoken);
       },
@@ -190,8 +248,14 @@ $(function () {
     });
   });
 });
+$(function () {
+  $(document).ready(function () {
+    $("#getevents").trigger("click");
+  });
+});
 // $(function () {
-//   if (!readCookie('adminview')) {
+//   isAdmin = parent.document.getElementsByClassName("bootstrapred");
+//      if (isAdmin.length===0) {
 //     $('#createbutton').hide();
 //     $('#eventdelete').hide();
 //     $('#eventedit').hide();
